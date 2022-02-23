791a19bff45869776eed2102e143b61806aa66c0
kimbongjune <nocdu112@naver.com>
2022-02-23 22:32:26 +0900

    redux-saga의 take, takeEverym takeLatest, throttle, delay차이

--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -1,4 +1,4 @@
-import {all, fork, call, put, take} from "redux-saga/effects"
+import {all, fork, call, put, take, takeEvery, takeLatest, throttle, delay} from "redux-saga/effects"
 import axios from "axios";
 
 function logInAPI(data){
@@ -7,7 +7,8 @@ function logInAPI(data){
 
 function* logIn(action){
     try {
-        const result = yield call(logInAPI, action.data);
+        //const result = yield call(logInAPI, action.data);
+        yield delay(1000);
         yield put({
             type : "LOG_IN_SUCCESS",
             data : result.data,
@@ -26,7 +27,8 @@ function logOutAPI(){
 
 function* logOut(){
     try {
-        const result = yield call(logOutAPI);
+        //const result = yield call(logOutAPI);
+        yield delay(1000);
         yield put({
             type : "LOG_OUT_SUCCESS",
             data : result.data,
@@ -45,7 +47,8 @@ function addPostAPI(data){
 
 function* addPost(action){
     try {
-        const result = yield call(addPostAPI, action.data);
+        //const result = yield call(addPostAPI, action.data);
+        yield delay(1000);
         yield put({
             type : "ADD_POST_SUCCESS",
             data : result.data,
@@ -58,16 +61,22 @@ function* addPost(action){
     }
 }
 
+//take는 한번 실행되면 사라짐
+//takeEvery는 while(true)와 동일함 여러번의 이벤트가 동시에 실행(더블클릭)되면 이벤트를 입력받은 만큼 실행
+//takeLatest는 여러번의 이벤트가 동시에 실행 되어도 마지막에 발생한 이벤트 한번만 실행 서버로 요청은 여러번 가지만 응답을 한번만 받음 새로고침하면 서버에는 두번의 요청이 들어갔기 때문에 서버에서 
+//반영을 해줘야한다.
+//throttle은 시간을 줘서 이벤트를 제어한다. 
+
 function* watchLogin(){
-    yield take("LOG_IN_REQUEST", logIn);
+    yield takeEvery("LOG_IN_REQUEST", logIn);
 }
 
 function* watchLogout(){
-    yield take("LOG_OUT_REQUEST", logOut);
+    yield takeEvery("LOG_OUT_REQUEST", logOut);
 }
 
 function* watchAddPost(){
-    yield take("ADD_POST_REQUEST", addPost);
+    yield takeEvery("ADD_POST_REQUEST", addPost);
 }
 
 
